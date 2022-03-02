964e1b7c6fa2a3e7b08d39da30383493376e1f7e
Witch-King of Angmar <100685110+SophrosyneEunoia@users.noreply.github.com>
2022-03-03 01:14:09 +0700

    Add files via upload

---
+++ concatenation_array.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def concat_array(nums : List[int]) -> List[int]:
+    """_summary_
+
+    >>> print(concat_array([1,2,1]))
+    [1, 2, 1, 1, 2, 1]
+    >>> print(concat_array([1,3,2,1]))
+    [1, 3, 2, 1, 1, 3, 2, 1]
+    """
+    nums2 = [nums[i] for i in range(len(nums))]
+    nums.extend(nums2)
+    return nums
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
