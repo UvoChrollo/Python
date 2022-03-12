ee56479369874fe0261e7e7f8631bac1ab9b9b73
SophrosyneEunoia <kelvinmulyawan2000@gmail.com>
2022-03-12 20:35:38 +0700

    add : mean.py

---
+++ Aing Macan Folder/mean.py
@@ -0,0 +1,28 @@
+from typing import List
+
+def mean(nums : List[int]) -> float:
+    """Fungsi manual untuk menghitung rata-rata
+
+    Args:
+        nums (List[int]): Data bilangan yang akan di inputkan
+
+    Returns:
+        float: rata-rata dari bilangan yang telah diinputkan
+
+    >>> print(mean([1, 2, 3]))
+    2.0
+
+    >>> print(mean(10))
+    TypeError: Hanya menerima List
+    """
+    if type(nums) != list:
+        raise TypeError("Hanya menerima List")
+
+    return sum(nums) / len(nums)
+
+a = 10
+print(mean(a))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
