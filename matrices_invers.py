d6d22d5307762438543af7842a123988a7d934b0
SophrosyneEunoia <kelvinmulyawan2000@gmail.com>
2022-03-15 23:21:36 +0700

    add : matrics_inverse.py

---
+++ matrices_invers.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def matrices_inversion(matrics : np.array) -> np.array:
+    """_summary_
+
+    >>> print(matrices_inversion(np.array([[1,2],[3,4]])))
+    [[-2. 1.],[1.5 -0.5]]
+    """
+    return np.linalg.inv(matrics)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
