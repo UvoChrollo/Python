2ca66d5ed26af61a818aced97d0e5b52d6aee89b
Witch-King of Angmar <100685110+SophrosyneEunoia@users.noreply.github.com>
2022-03-16 11:51:20 +0700

    Update matrices_invers.py

--- matrices_invers.py
+++ matrices_invers.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def matrices_inversion(matrics : np.array) -> np.array:
-    """_summary_
+    """Function to get invers by a matrices
 
     >>> print(matrices_inversion(np.array([[1,2],[3,4]])))
-    [[-2.  1. ]
-     [1.5 -0.5]]
+    [[-2.   1. ]
+     [ 1.5 -0.5]]
     """
     return np.linalg.inv(matrics)
 
