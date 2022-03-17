9a4270a02acd97c6bc58e56c9ef6226c1287e2eb
SophrosyneEunoia <kelvinmulyawan2000@gmail.com>
2022-03-18 00:28:05 +0700

    add : gaussian sum

---
+++ gaussian_sum.py
@@ -0,0 +1,13 @@
+def gaussian_sum(n : int) -> int:
+    """
+    >>> print(gaussian_sum(10))
+    55
+    
+    >>> print(gaussian_sum(100))
+    5050
+    """
+    return int(n*(n + 1)/2)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
